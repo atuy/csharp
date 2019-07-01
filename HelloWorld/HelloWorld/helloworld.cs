c88b98120d44436959b6e330c6ae776573407dc6
찬명 박 <pcm0028@naver.com>
2019-07-01 20:29:36 +0900

    ADD EX code

--- HelloWorld/HelloWorld/helloworld.cs
+++ HelloWorld/HelloWorld/helloworld.cs
@@ -10,12 +10,17 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            if (args.Length == 0)
+            String a;
+            a = ReadLine();
+            if (a.Length == 0)
             {
                 Console.WriteLine("사용법 : hellowrold.exe <이름>");
                 return;
             }
-            WriteLine("hello, {0}!", args[0]);
+            WriteLine("hello, {0}!", a);
+
+            WriteLine("여러분 안녕하세요?");
+            WriteLine("반갑습니다");
         }
     }
 }
