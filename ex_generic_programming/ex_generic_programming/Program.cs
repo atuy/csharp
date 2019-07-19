226cfa4925704ce38e4e6c4caad3c537564a1bf7
찬명 박 <pcm0028@naver.com>
2019-07-19 17:29:11 +0900

    add ex generic code

---
+++ ex_generic_programming/ex_generic_programming/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using static System.Console;
+
+namespace ex_generic_programming
+{
+    class Program
+    {
+        static void Copyarr<T>(T[] source, T[] target)
+        {
+            for (int i=0;i<source.Length;i++)
+            {
+                target[i] = source[i];
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] source = { 1, 2, 3, 4, 5, 6, 7 };
+            int[] target = new int[source.Length];
+
+            Copyarr<int>(source, target);
+
+            foreach(int num in target)
+            {
+                WriteLine(num);
+            }
+            string[] source2 = { "하나", "둘", "셋", "넷", "다섯", "여섯" };
+            string[] target2 = new string[source2.Length];
+
+            Copyarr<string>(source2, target2);
+            foreach (string num in target2)
+            {
+                WriteLine(num);
+            }
+        }
+    }
+}
---
+++ ex_generic_programming/gereric_class/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using static System.Console;
+
+namespace gereric_class
+{
+    class Sample<T>
+    {
+        private T[] arr;
+
+        public Sample()
+        {
+            arr = new T[3];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return arr[index];
+            }
+            set
+            {
+                if(index>=arr.Length)
+                {
+                    Array.Resize<T>(ref arr, index + 1);
+                    WriteLine($"사이즈 재설정 : {arr.Length}");
+                }
+                arr[index] = value;
+            }
+         }
+
+        public int Length
+        {
+            get
+            {
+                return arr.Length;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sample<string> str_list = new Sample<string>();
+            str_list[0] = "abc";
+            str_list[1] = "def";
+            str_list[2] = "ghi";
+            str_list[3] = "jkl";
+            str_list[4] = "mno";
+
+            for(int i=0;i<str_list.Length;i++)
+            {
+                WriteLine(str_list[i]);
+            }
+
+            WriteLine();
+
+            Sample<int> int_list = new Sample<int>();
+            int_list[0] = 1;
+            int_list[1] = 2;
+            int_list[2] = 3;
+            int_list[3] = 4;
+            int_list[4] = 5;
+
+            for (int i = 0; i < int_list.Length; i++)
+            {
+                WriteLine(int_list[i]);
+            }
+
+        }
+    }
+}
