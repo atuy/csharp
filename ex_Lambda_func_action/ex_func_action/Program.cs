7567d956df769534ffdf03433a506bff944c9703
찬명 박 <pcm0028@naver.com>
2019-07-25 22:40:55 +0900

    add ex lambda code

---
+++ ex_Lambda_func_action/ex_func_action/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using static System.Console;
+
+namespace ex_func_action
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int> func1 = () => 10;
+            WriteLine($"func1() = {func1()}");
+
+            Func<int, int> func2 = (x) => x * 2; 
+            WriteLine($"func2(4) = {func2(4)}");
+
+            Func<double, double, double> func3 = (x, y) => x / y;
+            WriteLine($"func3(22,7) = {func3(22,7)}");
+
+            Func<string, string> func4 = (c) => c.ToUpper();
+            WriteLine($"func4() = {func4("abcd")}");
+            WriteLine();
+
+            Action action1 = () => WriteLine("ACtion()");
+            action1();
+
+            int result = 0;
+            Action<int> action2 = (x) => result = x * x;
+            action2(3);
+            WriteLine($"result = {result}");
+
+            Action<double, double> action3 = (x, y) =>
+            {
+                double pi = x / y;
+                WriteLine($"Action<T1, T2>({x},{y}) : {pi}");
+            };
+            action3(22, 7);
+        }
+    }
+}
---
+++ ex_Lambda_func_action/ex_lambda/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+
+namespace ex_lambda
+{
+    class Program
+    {
+        delegate int calcul(int a, int b);
+        delegate string Concatenate(string[] args);
+        static void Main(string[] args)
+        {
+            calcul calcul = (a, b) => a + b;
+
+            WriteLine($"{3} + {4} = {calcul(3, 4)}");
+
+            Concatenate concatenate = (arr) =>
+            {
+                string result = "";
+                foreach (string s in arr)
+                {
+                    result += s;
+                }
+                return result;
+            };
+            WriteLine(concatenate(args));
+        }
+    }
+}
