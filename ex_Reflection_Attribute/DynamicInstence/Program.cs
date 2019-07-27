97788ded3a7050a0e4f053196cb08d9e2b8ec712
찬명 박 <pcm0028@naver.com>
2019-07-27 23:15:37 +0900

    add ex reflection and attribute code

---
+++ ex_Reflection_Attribute/DynamicInstence/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Reflection;
+using static System.Console;
+
+namespace DynamicInstence
+{
+    class Profile
+    {
+        private string name;
+        private string phone;
+
+        public Profile()
+        {
+            name = "";
+            phone = " ";
+        }
+        public Profile(string name, string phone)
+        {
+            this.name = name;
+            this.phone = phone;
+        }
+
+        public void Print()
+        {
+            WriteLine($"{name}, {phone}");
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type type = Type.GetType("DynamicInstence.Profile");
+            MethodInfo methodInfo = type.GetMethod("Print");
+            PropertyInfo nameproperty = type.GetProperty("Name");
+            PropertyInfo phoneproperty = type.GetProperty("Phone");
+
+            object profile = Activator.CreateInstance(type, "박찬명", "50621234");
+            methodInfo.Invoke(profile, null);
+
+            profile = Activator.CreateInstance(type);
+            nameproperty.SetValue(profile, "박찬호", null);
+            phoneproperty.SetValue(profile, "0123456789",null);
+
+            WriteLine($"{nameproperty.GetValue(profile, null)}, {phoneproperty.GetValue(profile, null)} ");
+        }
+    }
+}
---
+++ ex_Reflection_Attribute/ex_BasicAttribute/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+
+namespace ex_BasicAttribute
+{
+    class Sample
+    {
+        [Obsolete("Old메서드는 폐기되었습니다")]
+        public void Old()
+        {
+            WriteLine("Old()");
+        }
+        public void New()
+        {
+            WriteLine("New()");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sample sample = new Sample();
+            sample.Old();
+            sample.New();
+        }
+    }
+}
