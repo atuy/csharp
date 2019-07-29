69c67cfa594493388f3651e72a3a6a963f5fd6bb
찬명 박 <pcm0028@naver.com>
2019-07-29 20:27:23 +0900

    add ex dynamic code

---
+++ ex_dynamic/COMInterop/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace COMInterop
+{
+    class Program
+    {
+        public static void OldWay(string [,] date, string savePath)
+        {
+            Excel.Application excelApp = new Excel.Application();
+
+            excelApp.Workbooks.Add(Type.Missing);
+
+            Excel.Worksheet worksheet = (Excel.Worksheet)excelApp.ActiveSheet;
+
+            for(int i=0;i<date.GetLength(0);i++)
+            {
+                ((Excel.Range)worksheet.Cells[i + 1, 1]).Value2 = date[i, 0];
+                ((Excel.Range)worksheet.Cells[i + 1, 2]).Value2 = date[i, 1];
+            }
+
+            worksheet.SaveAs(savePath+ "\\shpark-book-old.xlsx", 
+                Type.Missing,
+                Type.Missing,
+                Type.Missing,
+                Type.Missing,
+                Type.Missing,
+                Type.Missing,
+                Type.Missing,
+                Type.Missing);
+
+            excelApp.Quit();
+        }
+
+        public static void NewWay(string[,] date, string savePath)
+        {
+            Excel.Application excelApp = new Excel.Application();
+
+            excelApp.Workbooks.Add();
+
+            Excel._Worksheet worksheet = excelApp.ActiveSheet;
+
+            for (int i = 0; i < date.GetLength(0); i++)
+            {
+                worksheet.Cells[i + 1, 1] = date[i, 0];
+                worksheet.Cells[i + 1, 2] = date[i, 1];
+            }
+
+            worksheet.SaveAs(savePath + "\\shpark-book-dynamic.xlsx");
+            excelApp.Quit();
+        }
+        static void Main(string[] args)
+        {
+            string savePath = System.IO.Directory.GetCurrentDirectory();
+            string[,] array = new string[,]
+            {
+                { "뇌를 자극하는 알고리즘", "2009" },
+                { "뇌를 자극하는 C# 4.0",   "2011" },
+                { "뇌를 자극하는 C# 5.0",   "2013" },
+                { "뇌를 자극하는 파이썬 3", "2016" },
+                { "이것이 C#이다",          "2018" }
+            };
+
+            Console.WriteLine("Creating Excel document in old way...");
+            OldWay(array, savePath);
+
+            Console.WriteLine("Creating Excel document in new way...");
+            NewWay(array, savePath);
+        }
+    }
+}
---
+++ ex_dynamic/ex_duck_typing/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using static System.Console;
+
+namespace ex_duck_typing
+{
+    class Duck
+    {
+        public void Walk()
+        {
+            WriteLine(this.GetType() + ".walk()");
+        }
+        public void Swim()
+        {
+            WriteLine(this.GetType() + ".Swim()");
+        }
+        public void Quack()
+        {
+            WriteLine(this.GetType() + ".Quack()");
+        }
+    }
+    class Mallard : Duck
+    {
+
+    }
+    class Robot
+    {
+        public void Walk()
+        {
+            WriteLine("Robot.walk()");
+        }
+        public void Swim()
+        {
+            WriteLine("Robot.Swim()");
+        }
+        public void Quack()
+        {
+            WriteLine("Robot .Quack()");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic[] arr = new dynamic[] { new Duck(), new Mallard(), new Robot() };
+
+            foreach(dynamic tmp in arr)
+            {
+                WriteLine(tmp.GetType());
+                tmp.Walk();
+                tmp.Swim();
+                tmp.Quack();
+                WriteLine();
+            }
+        }
+    }
+}
