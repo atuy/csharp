99c766ffe2da6d739ad80fef4552af1942c5134c
찬명 박 <pcm0028@naver.com>
2019-07-30 22:47:14 +0900

    add base File IO code

---
+++ ex_File_IO/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using static System.Console;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string directory;
+            string input = ReadLine();
+            if (input.Length < 1)
+                directory = ".";
+            else
+                directory = input;
+
+            Console.WriteLine($"{directory} directory Info");
+            Console.WriteLine("- Directories :");
+            var directories = (from dir in Directory.GetDirectories(directory)
+                               let info = new DirectoryInfo(dir)
+                               select new
+                               {
+                                   Name = info.Name,
+                                   Attributes = info.Attributes
+                               }).ToList();
+
+            foreach (var d in directories)
+                Console.WriteLine($"{d.Name} : {d.Attributes}");
+
+            Console.WriteLine("- Files :");
+            var files = (from file in Directory.GetFiles(directory)
+                         let info = new FileInfo(file)
+                         select new
+                         {
+                             Name = info.Name,
+                             FileSize = info.Length,
+                             Attributes = info.Attributes
+                         }).ToList();
+            foreach (var f in files)
+                Console.WriteLine(
+                    $"{f.Name} : {f.FileSize}, {f.Attributes}");
+        }
+    }
+}
