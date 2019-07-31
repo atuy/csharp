9d2bf96586a31e3b9ae895ffd2a58631b68423ae
찬명 박 <pcm0028@naver.com>
2019-07-31 21:01:12 +0900

    add ex File IO code

---
+++ ex_File_IO/BasicIO/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using static System.Console;
+namespace BasicIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long someValue = 0x123456789ABCDEF0;
+            WriteLine("Original date: 0x{0:X16}", someValue );
+
+            Stream output = new FileStream("test.dat", FileMode.Create);
+            byte[] wbytes = BitConverter.GetBytes(someValue);
+
+            Write("{0,-13} : ", "Byte array");
+
+            foreach(byte i in wbytes)
+            {
+                Write("{0:x2} ", i);
+            }
+            WriteLine();
+
+            output.Write(wbytes, 0, wbytes.Length);
+            output.Close();
+
+            Stream input = new FileStream("test.dat", FileMode.Open);
+            byte[] rbyte = new byte[8];
+
+            int n = 0;
+            while( input .Position <input.Length)
+            {
+                rbyte[n++] = (byte)input.ReadByte();
+            }
+
+            long read = BitConverter.ToInt64(rbyte, 0);
+            WriteLine("{0,-13}  : 0x{1:X16} ", "read date", read);
+            input.Close();
+        }
+    }
+}
---
+++ ex_File_IO/TextFile/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using static System.Console;
+
+namespace TextFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter input = new StreamWriter(new FileStream("test.txt", FileMode.Create));
+
+            input.WriteLine(int.MaxValue);
+            input.WriteLine("good morning");
+            input.WriteLine(uint.MaxValue);
+            input.WriteLine("안녕하세요");
+            input.WriteLine(double.MinValue);
+
+            input.Close();
+
+            StreamReader output = new StreamReader(new FileStream("test.txt", FileMode.Open));
+
+            WriteLine($"File Size : {output.BaseStream.Length} byte");
+
+            while (output.EndOfStream == false)
+            {
+                WriteLine(output.ReadLine());
+            }
+
+            output.Close();
+        }
+    }
+}
