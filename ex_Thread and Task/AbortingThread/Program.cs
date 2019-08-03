ea81f2ec18988829f157bb99de84a3b3f48fb522
찬명 박 <pcm0028@naver.com>
2019-08-03 21:24:06 +0900

    add ex using code

---
+++ ex_Thread and Task/AbortingThread/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+using static System.Console;
+
+namespace AbortingThread
+{
+    class SideTAsk
+    {
+        int count;
+        public SideTAsk(int count)
+        {
+            this.count = count;
+        }
+        public void KeepAlive()
+        {
+            try
+            {
+                while(count > 0)
+                {
+                    WriteLine($"{count--} left");
+                    Thread.Sleep(10);
+                }
+                WriteLine("count : 0");
+            }
+            catch (ThreadAbortException e)
+            {
+                WriteLine(e);
+                Thread.ResetAbort();
+            }
+            finally
+            {
+                WriteLine("Clearing");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SideTAsk task = new SideTAsk(100);
+            Thread t1 = new Thread(new ThreadStart(task.KeepAlive));
+            t1.IsBackground = false;
+
+            WriteLine("start");
+            t1.Start();
+            Thread.Sleep(10);
+
+            WriteLine("Aborting thread");
+            t1.Abort();
+
+            WriteLine("wating until thread stops");
+            t1.Join();
+
+            WriteLine("fin");
+        }
+    }
+}
--- ex_Thread and Task/basicthread/Program.cs
+++ ex_Thread and Task/basicthread/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Threading;
+using static System.Console;
 
 namespace basicthread
 {
     class Program
     {
+        static void Something()
+        {
+            for (int i=0;i<10;i++)
+            {
+                WriteLine($"something : {i}");
+                Thread.Sleep(100);
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Thread t1 = new Thread(new ThreadStart(Something));
+            WriteLine("start");
+
+            t1.Start();
+            for (int i = 0; i<10;i++)
+            {
+                WriteLine($"main : {i}");
+                Thread.Sleep(100);
+            }
+            WriteLine("wating until thread stops");
+            t1.Join();
+
+            WriteLine("Fin");
         }
     }
 }
